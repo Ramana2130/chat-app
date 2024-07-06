5349468dabd8c3350ac8f9d4bd248157acd96616
Ramana2130 <ramanaraj7143@gmail.com>
2024-07-06 12:24:01 +0530

    add

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -6,53 +6,45 @@ export const signupUser = async (req, res) => {
     const { fullName, username, password, confirmPassword, gender } = req.body;
 
     if (password !== confirmPassword) {
-      return res.status(400).json({ error: "Password does not match" });
+      return res.status(400).json({ error: "Password does'nt Match" });
     }
 
-    // Check if username already exists
-    const existingUser = await User.findOne({ username });
-    if (existingUser) {
+    const user = await User.findOne({ username });
+
+    if (user) {
       return res.status(400).json({ error: "Username already exists" });
     }
 
-    // Hash password
+    //hash password
     const salt = await bcrypt.genSalt(10);
     const hashPassword = await bcrypt.hash(password, salt);
+    //profile
+    const boyProfile = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfile = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
-    // Prepare profile picture URL based on gender
-    const profilePicUrl =
-      gender === "male"
-        ? `https://avatar.iran.liara.run/public/boy?username=${username}`
-        : `https://avatar.iran.liara.run/public/girl?username=${username}`;
-
-    // Create new user instance
     const newUser = new User({
       fullName,
       username,
       password: hashPassword,
       gender,
-      profile_pic: profilePicUrl,
+      profile_pic: gender === "male" ? boyProfile : girlProfile,
     });
 
-    // Save new user to database
-    await newUser.save();
-
-    // Generate token and set cookie
-    await generateTokenAndCookie(newUser._id, res);
-
-    // Respond with user data
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      username: newUser.username,
-      profile_pic: newUser.profile_pic,
-    });
-  } catch (error) {
-    console.error("Error in signup controller:", error.message);
-    // Handle specific MongoDB connection timeout error
-    if (error.name === "MongoTimeoutError") {
-      return res.status(500).json({ error: "Database connection timed out" });
+    if (newUser) {
+      //generate token
+      await generateTokenAndCookie(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        username: newUser.username,
+        profile_pic: newUser.profile_pic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid User data" });
     }
+  } catch (error) {
+    console.log("error in signup controller", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
