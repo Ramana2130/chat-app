09e2c37f7561c2101142cbef263ca4775e5ddb4c
Ramana2130 <ramanaraj7143@gmail.com>
2024-07-06 20:31:59 +0530

    added

--- frontend/src/hooks/userLogin.js
+++ frontend/src/hooks/userLogin.js
@@ -1,55 +1,55 @@
-import { useState } from "react"
+import { useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
 import toast from "react-hot-toast";
 
+const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
-const useLogin = () =>{
-    const [loading, setLoading] = useState(false);
-    const {setAuthUser} = useAuthContext()
-    const login = async (username, password) => {
-        const success = handleInputError({
-            username,
-            password,
-          });
-      
-          if (!success) return;
-        setLoading(true)
-        try {
-            const res = await fetch("api/auth/login" ,{
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body : JSON.stringify({username, password})
-            })
+  const login = async (username, password) => {
+    const success = handleInputError({
+      username,
+      password,
+    });
 
-            const data = await res.json();
-            if(data.error) {
-                throw new Error(data.error)
-            }
+    if (!success) return;
 
-            localStorage.setItem("chat-app", JSON.stringify(data));
-            setAuthUser(data)
-            toast.success("Login Successfully")
-        } catch (error) {
-            toast.error(error.message)
-        }finally {
-            setLoading(false)
-        }
+    setLoading(true);
+    try {
+      const res = await fetch("api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await res.json();
+      if (data.error) {
+        throw new Error(data.error);
+      }
+
+      // Store only the necessary information in localStorage
+      localStorage.setItem(
+        "chat-app",
+        JSON.stringify({ username: data.username, token: data.token })
+      );
+      setAuthUser(data);
+      toast.success("Login Successfully");
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return {loading, login}
-}
+  return { loading, login };
+};
 
-export default useLogin
+export default useLogin;
 
-
-function handleInputError({
-    username,
-    password,
-  }) {
-    if ( !username || !password ) {
-      toast.error("Please Fill in all details");
-      return false;
-    }
-    return true;
+function handleInputError({ username, password }) {
+  if (!username || !password) {
+    toast.error("Please Fill in all details");
+    return false;
   }
-  
+  return true;
+}
