86d18005e787e275b65329b0b704a39fc7ba8a10
Ramana2130 <ramanaraj7143@gmail.com>
2024-07-06 12:02:19 +0530

    Depolyment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -8,17 +9,23 @@ import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDb.js";
 import { app, server } from "./socket/socket.js";
 
-
-
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
